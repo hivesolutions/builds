31f57e5b5b550dd96de81774e01946034d1458a5
João Magalhães <joamag@hive.pt>
2013-03-02 20:09:18 +0000

    Update php.sh

--- rasp_pi/php.sh
+++ rasp_pi/php.sh
@@ -5,15 +5,15 @@ tar -zxf "php-$VERSION.tar.gz"
 rm -f "php-$VERSION.tar.gz"
 cd php-$VERSION
 
-export PATH=/opt/arm-unknown-linux-gnueabi/bin:$PATH
-export CFLAGS="$CFLAGS -I/opt/arm-unknown-linux-gnueabi/include\
-    -L/opt/arm-unknown-linux-gnueabi/lib\
-    -R/opt/arm-unknown-linux-gnueabi/lib\
-    -L/opt/arm-unknown-linux-gnueabi/usr/lib\
-    -R/opt/arm-unknown-linux-gnueabi/usr/lib\
-    -L/opt/arm-unknown-linux-gnueabi/arm-unknown-linux-gnueabi/sysroot/usr/lib\
-    -R/opt/arm-unknown-linux-gnueabi/arm-unknown-linux-gnueabi/sysroot/usr/lib"
-./configure --host=arm-unknown-linux-gnueabi --build=arm --prefix=/opt/arm-unknown-linux-gnueabi\
-    -enable-embed=static --enable-bcmath --disable-phar --without-pear --without-iconv\
-    --with-config-file-path=/usr/lib
+export PATH=$PREFIX/bin:$PATH
+export CFLAGS="$CFLAGS -I$PREFIX/include\
+    -L$PREFIX/lib\
+    -R$PREFIX/lib\
+    -L$PREFIX/usr/lib\
+    -R$PREFIX/usr/lib\
+    -L$PREFIX/$HOST/sysroot/usr/lib\
+    -R$PREFIX/$HOST/sysroot/usr/lib"
+./configure --host=$HOST --build=$BUILD --prefix=$PREFIX\
+    -enable-embed=static --enable-bcmath --disable-phar\
+    --without-pear --without-iconv --with-config-file-path=/usr/lib
 make && make install
