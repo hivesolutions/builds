443ea0d5260216e6a8d6ac92830055009f2962cb
João Magalhães <joamag@hive.pt>
2013-03-02 20:14:31 +0000

    Update all.sh

--- rasp_pi/all.sh
+++ rasp_pi/all.sh
@@ -5,6 +5,10 @@ cd build
 ../cross.sh
 
 export CFLAGS="-O3"
+export HOST="arm-rasp-linux-gnueabi"
+export CROSS="arm-rasp-linux-"
+export BUILD="arm"
+export PREFIX=/opt/$HOST
 
 ../openssl.sh
 ../pcre.sh
