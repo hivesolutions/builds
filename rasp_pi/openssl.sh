6e42712e5566d340a5d2cf904cf3bca38328e6aa
João Magalhães <joamag@hive.pt>
2013-03-02 20:02:17 +0000

    Update openssl.sh

--- rasp_pi/openssl.sh
+++ rasp_pi/openssl.sh
@@ -5,10 +5,9 @@ tar -zxf "openssl-$VERSION.tar.gz"
 rm -f "openssl-$VERSION.tar.gz"
 cd openssl-$VERSION
 
-export cross=arm-unknown-linux-
-export PATH=/opt/arm-unknown-linux-gnueabi/bin:$PATH
-./config --prefix=/opt/arm-unknown-linux-gnueabi
-./Configure dist --prefix=/opt/arm-unknown-linux-gnueabi
-make CC="arm-unknown-linux-gnueabi-gcc" AR="arm-unknown-linux-gnueabi-ar r"\
-    RANLIB="arm-unknown-linux-gnueabi-ranlib"
+export cross=$CROSS
+export PATH=$PREFIX/bin:$PATH
+./config --prefix=$PREFIX
+./Configure dist --prefix=$PREFIX
+make CC="$HOST-gcc" AR="$HOST-ar r" RANLIB="$HOST-ranlib"
 make install
