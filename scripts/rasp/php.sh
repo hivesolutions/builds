8015549bbd33d240dfca7b0b5152c7aa284ae72e
joamag <joamag@gmail.com>
2013-03-04 19:02:53 +0000

    removed imap

--- scripts/rasp/php.sh
+++ scripts/rasp/php.sh
@@ -22,8 +22,8 @@ cd -
 ./configure --host=$HOST --build=$BUILD --prefix=$PREFIX\
     -enable-embed=static --enable-bcmath --enable-mysqlnd\
     --disable-phar --with-mysql --with-mysqli --with-gd --with-openssl\
-    --with-imap --with-imap-ssl --without-pear --without-iconv\
-    --with-mysql=mysqlnd --with-pdo-mysql=mysqlnd --with-config-file-path=/usr/lib\
-    --with-zip=$PREFIX --with-bz2=$PREFIX --with-zlib-dir=$PREFIX\
-    --with-freetype-dir=$PREFIX --with-png-dir=$PREFIX --with-jpeg-dir=$PREFIX
+    --without-pear --without-iconv --with-mysql=mysqlnd --with-pdo-mysql=mysqlnd\
+    --with-config-file-path=/usr/lib --with-zip=$PREFIX --with-bz2=$PREFIX\
+    --with-zlib-dir=$PREFIX --with-freetype-dir=$PREFIX --with-png-dir=$PREFIX\
+    --with-jpeg-dir=$PREFIX
 make && make install
