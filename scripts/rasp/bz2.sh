abf59465b880c2c6a4bf6879cb06c4d19247ab13
joamag <joamag@gmail.com>
2013-03-04 18:44:04 +0000

    new bz2 file

---
+++ scripts/rasp/bz2.sh
@@ -0,0 +1,10 @@
+VERSION="1.0.6"
+
+wget -q "http://www.bzip.org/$VERSION/bzip2-$VERSION.tar.gz"
+tar -zxf "bzip2-$VERSION.tar.gz"
+rm -f "bzip2-$VERSION.tar.gz"
+cd bzip2-$VERSION
+
+export PATH=$PREFIX/bin:$PATH
+make CC=$HOST-gcc CFLAGS="$CFLAGS"
+make install PREFIX=$PREFIX
