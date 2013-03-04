bf76f562231be6230e62ed65b4c3a1cb3586bfbd
joamag <joamag@gmail.com>
2013-03-04 17:30:37 +0000

    small file change

--- scripts/rasp/libjpeg.sh
+++ scripts/rasp/libjpeg.sh
@@ -2,7 +2,7 @@ VERSION="8d"
 
 wget -q "http://www.ijg.org/files/jpegsrc.v$VERSION.tar.gz"
 tar -zxf "jpegsrc.v$VERSION.tar.gz"
-rm -f "jpeg-$VERSION.tar.gz"
+rm -f "jpegsrc.v$VERSION.tar.gz"
 cd jpeg-$VERSION
 
 export PATH=$PREFIX/bin:$PATH
