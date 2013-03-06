2d0185b1e46df703102f4a409da944a1f723b837
joamag <joamag@gmail.com>
2013-03-06 11:52:06 +0000

    new base directives support

--- scripts/rasp/all.sh
+++ scripts/rasp/all.sh
@@ -4,11 +4,7 @@ cd build
 
 ../cross.sh
 
-export CFLAGS="-O3 -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s"
-export HOST="arm-rasp-linux-gnueabi"
-export CROSS="arm-rasp-linux-"
-export BUILD="arm"
-export PREFIX=/opt/$HOST
+source ../base.sh
 
 ../bin.sh
 ../openssl.sh
---
+++ scripts/rasp/base.sh
@@ -0,0 +1,5 @@
+export CFLAGS="-O3 -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s"
+export HOST="arm-rasp-linux-gnueabi"
+export CROSS="arm-rasp-linux-"
+export BUILD="arm"
+export PREFIX=/opt/$HOST
