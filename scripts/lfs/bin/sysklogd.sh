e164c3d03a080fa9aaaa13736d5ef02debe2b972
João Magalhães <joamag@hive.pt>
2013-03-07 01:08:25 +0000

    new scripts added

---
+++ scripts/lfs/bin/sysklogd.sh
@@ -0,0 +1,23 @@
+VERSION="1.5"
+tar -zxf "sysklogd-$VERSION.tar.gz"
+cd sysklogd-$VERSION
+
+make
+make BINDIR=/sbin install
+
+cat > /etc/syslog.conf << "EOF"
+# Begin /etc/syslog.conf
+
+auth,authpriv.* -/var/log/auth.log
+*.*;auth,authpriv.none -/var/log/sys.log
+daemon.* -/var/log/daemon.log
+kern.* -/var/log/kern.log
+mail.* -/var/log/mail.log
+user.* -/var/log/user.log
+*.emerg *
+
+# End /etc/syslog.conf
+EOF
+
+cd ..
+rm -rf sysklogd-$VERSION
