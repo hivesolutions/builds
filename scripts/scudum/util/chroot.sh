934229cb70136103ef6485fd196cdb297ecc3ab5
joamag <joamag@gmail.com>
2013-03-08 16:49:06 +0000

    new script code

--- scripts/scudum/util/chroot.sh
+++ scripts/scudum/util/chroot.sh
@@ -1,7 +1,12 @@
 DEV_NAME=${DEV_NAME-/dev/sdb}
+DEV_BOOT=${DEV_BOOT-"$DEV_NAME"1}
+DEV_ROOT=${DEV_ROOT-"$DEV_NAME"3}
 SCUDUM=${SCUDUM-/mnt/scudum}
 
-mnt -v $DEV_NAME $SCUDUM
+mkdir -pv $SCUDUM
+mount -v $DEV_ROOT $SCUDUM
+mkdir -pv $SCUDUM/boot
+mount -v $DEV_BOOT $SCUDUM/boot
 
 chroot $SCUDUM /usr/bin/env -i\
     HOME=/root TERM="$TERM" PS1='\u:\w\$ '\
