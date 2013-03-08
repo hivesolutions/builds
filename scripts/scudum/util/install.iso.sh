e3fb898dd8770f3c3cfa1b511e4e847251652ea6
joamag <joamag@gmail.com>
2013-03-08 13:27:38 +0000

    small code fix

--- scripts/scudum/util/install.iso.sh
+++ scripts/scudum/util/install.iso.sh
@@ -4,6 +4,7 @@ FILE=${FILE-scudum.raw}
 SIZE=${SIZE-4294967296}
 BLOCK_SIZE=${BLOCK_SIZE-4096}
 SIZE_B=$(expr $SIZE / $BLOCK_SIZE)
+DIR=$(dirname $(readlink -f $0))
 
 echo "Creating raw file '$FILE' with $SIZE_B blocks..."
 
@@ -14,6 +15,6 @@ kpartx -a $DEV_NAME
 
 echo "Created raw file and set device at '$DEV_NAME'"
 
-DRIVE=$DEV_NAME ./install.sh
+DRIVE=$DEV_NAME $DIR/install.sh
 
 losetup -d $DEV_NAME
