6084f03c4acc49d62b4fe984558820e1cdfeb0f7
joamag <joamag@gmail.com>
2013-03-08 16:50:28 +0000

    new script code

--- scripts/scudum/util/chroot.sh
+++ scripts/scudum/util/chroot.sh
@@ -8,7 +8,20 @@ mount -v $DEV_ROOT $SCUDUM
 mkdir -pv $SCUDUM/boot
 mount -v $DEV_BOOT $SCUDUM/boot
 
+mount -v --bind /dev $SCUDUM/dev
+mount -vt devpts devpts $SCUDUM/dev/pts
+mount -vt proc proc $SCUDUM/proc
+mount -vt sysfs sysfs $SCUDUM/sys
+
 chroot $SCUDUM /usr/bin/env -i\
     HOME=/root TERM="$TERM" PS1='\u:\w\$ '\
     PATH=/bin:/usr/bin:/sbin:/usr/sbin\
     /bin/bash --login
+
+umount -v $SCUDUM/sys
+umount -v $SCUDUM/proc
+umount -v $SCUDUM/dev/pts
+umount -v $SCUDUM/dev
+
+umount -v $SCUDUM/boot && rm -rvf $SCUDUM/boot
+umount -v $SCUDUM && rm -rvf $SCUDUM
