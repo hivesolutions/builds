89de8c7b265324366ac20e923aa0904aaeb98c10
joamag <joamag@gmail.com>
2013-03-08 17:34:16 +0000

    new build script added

---
+++ scripts/scudum/util/build.sh
@@ -0,0 +1,26 @@
+DEV_NAME=${DEV_NAME-/dev/sdb}
+DEV_BOOT=${DEV_BOOT-"$DEV_NAME"1}
+DEV_ROOT=${DEV_ROOT-"$DEV_NAME"3}
+SCUDUM=${SCUDUM-/mnt/scudum}
+FILE=${FILE-scudum.tar.gz}
+
+BASE=$(pwd)
+
+mkdir -pv $SCUDUM
+mount -v $DEV_ROOT $SCUDUM
+mkdir -pv $SCUDUM/boot
+mount -v $DEV_BOOT $SCUDUM/boot
+
+cd $SCUDUM/root
+rm -rf *
+rm -rf .*
+
+cd $SCUDUM/tmp
+rm -rf *
+rm -rf .*
+
+cd $SCUDUM
+tar -zcvf $BASE/$FILE *
+
+umount -v $SCUDUM/boot && rm -rvf $SCUDUM/boot
+umount -v $SCUDUM && rm -rvf $SCUDUM
