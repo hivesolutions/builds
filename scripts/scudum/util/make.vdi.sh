4cd4d36dcf06e698a55ad0d207d7678f3ba2797e
João Magalhães <joamag@hive.pt>
2013-03-10 11:57:38 +0000

    small code change

--- scripts/scudum/util/make.vdi.sh
+++ scripts/scudum/util/make.vdi.sh
@@ -3,6 +3,7 @@ FILE_OUT=${FILE_IN-scudum.vdi}
 
 DIR=$(dirname $(readlink -f $0))
 
-FILE=$FILE_IN $DIR/install.img.sh
+FILE=$FILE_IN $DIR/make.img.sh
 
 VBoxManage convertdd $FILE_IN $FILE_OUT
+rm -v $FILE_IN
