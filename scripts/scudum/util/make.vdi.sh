54d9596e783041ca43dc2b94f34ffc152900a8e7
João Magalhães <joamag@hive.pt>
2013-03-10 12:00:01 +0000

    small change

--- scripts/scudum/util/make.vdi.sh
+++ scripts/scudum/util/make.vdi.sh
@@ -1,10 +1,13 @@
 FILE_IN=${FILE_IN-scudum.img}
 FILE_OUT=${FILE_IN-scudum.vdi}
 DEV_NAME=${DEV_NAME-/dev/null}
+BOOT_SIZE=${BOOT_SIZE-+1G}
+SWAP_SIZE=${SWAP_SIZE-+2G}
 
 DIR=$(dirname $(readlink -f $0))
 
-FILE=$FILE_IN DEV_NAME=$DEV_NAME $DIR/make.img.sh
+FILE=$FILE_IN DEV_NAME=$DEV_NAME BOOT_SIZE=$BOOT_SIZE\
+    SWAP_SIZE=$SWAP_SIZE $DIR/make.img.sh
 
 VBoxManage convertdd $FILE_IN $FILE_OUT
 rm -v $FILE_IN
