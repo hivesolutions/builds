c47efe020114b4c30fc0a6afdd2e3ef82a126bdd
joamag <joamag@gmail.com>
2013-03-15 17:33:17 +0000

    new support for make iso

--- scripts/scudum/util/make.iso.sh
+++ scripts/scudum/util/make.iso.sh
@@ -1,50 +1,66 @@
-FILE=${FILE-scudum.img}
-SIZE=${SIZE-4294967296}
-OFFSET=${OFFSET-1048576}
-BLOCK_SIZE=${BLOCK_SIZE-4096}
-BOOT_SIZE=${BOOT_SIZE-1073741824}
-SWAP_SIZE=${SWAP_SIZE-2147483648}
-BOOT_SIZE_F=${BOOT_SIZE_F-+1G}
-SWAP_SIZE_F=${SWAP_SIZE_F-+2G}
-SLEEP_TIME=3
+FILE=${FILE-scudum.iso}
+DEV_NAME=${DEV_NAME-/dev/null}
+BOOT_SIZE=${BOOT_SIZE-+1G}
+SWAP_SIZE=${SWAP_SIZE-+2G}
+SCUDUM=${SCUDUM-/tmp/scudum}
+REBUILD=${SCUDUM-0}
 
-SIZE_B=$(expr $SIZE / $BLOCK_SIZE)
 DIR=$(dirname $(readlink -f $0))
 
-echo "Creating raw file '$FILE' with $SIZE_B blocks..."
+if [ "$REBUILD" == "1" ]; then
+    dd if=/dev/zero of=$DEV_NAME bs=1M
 
-dd if=/dev/zero of=$FILE bs=$BLOCK_SIZE count=$SIZE_B
+    DEV_NAME=$DEV_NAME BOOT_SIZE=$BOOT_SIZE SWAP_SIZE=$SWAP_SIZE\
+        SCUDUM=$SCUDUM $DIR/install.dev.sh
+fi
 
-echo "Creating and allocating partitions..."
+mkdir -pv $SCUDUM
+mount -v $DEV_ROOT $SCUDUM
+if [ $DEV_ROOT != $DEV_BOOT ]; then
+    mkdir -pv $SCUDUM/boot
+    mount -v $DEV_BOOT $SCUDUM/boot
+fi
 
-(echo n; echo p; echo 1; echo ; echo $BOOT_SIZE_F; echo a; echo 1; echo w) | fdisk $FILE
-sleep $SLEEP_TIME
-(echo n; echo p; echo 2; echo ; echo $SWAP_SIZE_F; echo t; echo 2; echo 82; echo w) | fdisk $FILE
-sleep $SLEEP_TIME
-(echo n; echo p; echo 3; echo ; echo ; echo w) | fdisk $FILE
-sleep $SLEEP_TIME
+rm -rf ramdisk
+dd if=/dev/zero of=ramdisk bs=1k count=32768
+losetup /dev/loop1 ramdisk
 
-DEV_NAME=$(losetup -f --show $FILE)
-DEV_INDEX=${DEV_NAME:${#DEV_NAME} - 1}
-DEV_BOOT=/dev/loop$(expr $DEV_INDEX + 1)
-DEV_SWAP=/dev/loop$(expr $DEV_INDEX + 2)
-DEV_ROOT=/dev/loop$(expr $DEV_INDEX + 3)
+mke2fs /dev/loop1
+mkdir -p /mnt/loop1
 
-BOOT_OFFSET=$(expr $OFFSET)
-SWAP_OFFSET=$(expr $BOOT_OFFSET + $BOOT_SIZE)
-ROOT_OFFSET=$(expr $SWAP_OFFSET + $SWAP_SIZE)
+mount /dev/loop1 /mnt/loop1 
+rm -rf /mnt/loop1/lost+found 
+cp -dpR $DEV_ROOT/initrd/* /mnt/loop1/
 
-losetup -o $BOOT_OFFSET $DEV_BOOT $DEV_NAME
-losetup -o $SWAP_OFFSET $DEV_SWAP $DEV_NAME
-losetup -o $ROOT_OFFSET $DEV_ROOT $DEV_NAME
+umount /mnt/loop1
+losetup -d /dev/loop1
 
-echo "Created raw file and set device at '$DEV_NAME'"
+rm -rf /mnt/loop1
 
-DEV_NAME=$DEV_NAME DEV_BOOT=$DEV_BOOT DEV_SWAP=$DEV_SWAP DEV_ROOT=$DEV_ROOT $DIR/install.sh
+gzip -9 -c ramdisk > $DEV_ROOT/isolinux/initrd.img
 
-losetup -d $DEV_ROOT
-losetup -d $DEV_SWAP
-losetup -d $DEV_BOOT
-sleep $SLEEP_TIME
+rm -rf ramdisk
 
-losetup -d $DEV_NAME
+cd $DEV_ROOT
+tar -zcf images/root.tar.gz root
+tar -zcf images/dev.tar.gz dev
+tar -zcf images/etc.tar.gz etc
+cd ..
+
+mkisofs -r -o /mnt/extra/scudum/scudum.iso \
+   -b isolinux/isolinux.bin -c isolinux/boot.cat \
+   -no-emul-boot -boot-load-size 4 -boot-info-table \
+   $DEV_ROOT
+
+rm -v $DEV_ROOT/images/root.tar.gz
+rm -v $DEV_ROOT/images/dev.tar.gz
+rm -v $DEV_ROOT/images/etc.tar.gz
+
+if [ $DEV_ROOT != $DEV_BOOT ]; then
+    umount -v $SCUDUM/boot && rm -rvf $SCUDUM/boot
+fi
+umount -v $SCUDUM && rm -rvf $SCUDUM
+
+if [ "$REBUILD" == "1" ]; then
+    dd if=/dev/zero of=$DEV_NAME bs=1M
+fi
