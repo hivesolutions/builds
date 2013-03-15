1a8485ce3d6cf0978b43875def89924197dc5400
joamag <joamag@gmail.com>
2013-03-15 17:54:17 +0000

    new rebuild support

--- scripts/scudum/util/make.img.sh
+++ scripts/scudum/util/make.img.sh
@@ -2,13 +2,19 @@ FILE=${FILE-scudum.img}
 DEV_NAME=${DEV_NAME-/dev/null}
 BOOT_SIZE=${BOOT_SIZE-+1G}
 SWAP_SIZE=${SWAP_SIZE-+2G}
+REBUILD=${REBUILD-0}
 
 DIR=$(dirname $(readlink -f $0))
 
-dd if=/dev/zero of=$DEV_NAME bs=1M
+if [ "$REBUILD" == "1" ]; then
+    dd if=/dev/zero of=$DEV_NAME bs=1M
 
-DEV_NAME=$DEV_NAME BOOT_SIZE=$BOOT_SIZE\
-    SWAP_SIZE=$SWAP_SIZE $DIR/install.dev.sh
+    DEV_NAME=$DEV_NAME BOOT_SIZE=$BOOT_SIZE\
+        SWAP_SIZE=$SWAP_SIZE $DIR/install.dev.sh
+fi
 
 dd if=$DEV_NAME of=$FILE bs=1M
-dd if=/dev/zero of=$DEV_NAME bs=1M
+
+if [ "$REBUILD" == "1" ]; then
+    dd if=/dev/zero of=$DEV_NAME bs=1M
+fi
--- scripts/scudum/util/make.vdi.sh
+++ scripts/scudum/util/make.vdi.sh
@@ -3,11 +3,12 @@ FILE_OUT=${FILE_IN-scudum.vdi}
 DEV_NAME=${DEV_NAME-/dev/null}
 BOOT_SIZE=${BOOT_SIZE-+1G}
 SWAP_SIZE=${SWAP_SIZE-+2G}
+REBUILD=${REBUILD-0}
 
 DIR=$(dirname $(readlink -f $0))
 
 FILE=$FILE_IN DEV_NAME=$DEV_NAME BOOT_SIZE=$BOOT_SIZE\
-    SWAP_SIZE=$SWAP_SIZE $DIR/make.img.sh
+    SWAP_SIZE=$SWAP_SIZE REBUILD=$REBUILD $DIR/make.img.sh
 
 VBoxManage convertdd $FILE_IN $FILE_OUT
 rm -v $FILE_IN
