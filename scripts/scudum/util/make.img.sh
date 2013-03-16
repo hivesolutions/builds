464d4f567943ce3e0e21166a56b184741524c42c
João Magalhães <joamag@hive.pt>
2013-03-16 01:06:09 +0000

    Update make.img.sh

--- scripts/scudum/util/make.img.sh
+++ scripts/scudum/util/make.img.sh
@@ -2,6 +2,7 @@ FILE=${FILE-scudum.img}
 DEV_NAME=${DEV_NAME-/dev/null}
 BOOT_SIZE=${BOOT_SIZE-+1G}
 SWAP_SIZE=${SWAP_SIZE-+2G}
+LOADER=${LOADER-grub}
 REBUILD=${REBUILD-0}
 
 DIR=$(dirname $(readlink -f $0))
@@ -10,7 +11,7 @@ if [ "$REBUILD" == "1" ]; then
     dd if=/dev/zero of=$DEV_NAME bs=1M
 
     DEV_NAME=$DEV_NAME BOOT_SIZE=$BOOT_SIZE\
-        SWAP_SIZE=$SWAP_SIZE $DIR/install.dev.sh
+        SWAP_SIZE=$SWAP_SIZE LOADER=$LOADER $DIR/install.dev.sh
 fi
 
 dd if=$DEV_NAME of=$FILE bs=1M
