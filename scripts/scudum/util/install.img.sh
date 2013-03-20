1f71799b4bc9849bdaf8f5b587d0c3a2ba883ea0
joamag <joamag@gmail.com>
2013-03-20 13:59:36 +0000

    new simplified kpartx version

--- scripts/scudum/util/install.img.sh
+++ scripts/scudum/util/install.img.sh
@@ -3,9 +3,12 @@
 
 FILE=${FILE-scudum.img}
 SIZE=${SIZE-2147483648}
+OFFSET=${OFFSET-1048576}
 BLOCK_SIZE=${BLOCK_SIZE-4096}
-BOOT_SIZE=${BOOT_SIZE_F-+512M}
-SWAP_SIZE=${SWAP_SIZE_F-+64M}
+BOOT_SIZE=${BOOT_SIZE-536870912}
+SWAP_SIZE=${SWAP_SIZE-67108864}
+BOOT_SIZE_F=${BOOT_SIZE_F-+512M}
+SWAP_SIZE_F=${SWAP_SIZE_F-+64M}
 SCHEMA=${SCHEMA-stored}
 LOADER=${LOADER-grub}
 SLEEP_TIME=3
@@ -15,23 +18,35 @@ DIR=$(dirname $(readlink -f $0))
 
 dd if=/dev/zero of=$FILE bs=$BLOCK_SIZE count=$SIZE_B
 
-(echo n; echo p; echo 1; echo ; echo $BOOT_SIZE; echo a; echo 1; echo w) | fdisk -H 255 -S 63 $FILE
+(echo n; echo p; echo 1; echo ; echo $BOOT_SIZE_F; echo a; echo 1; echo w) | fdisk -H 255 -S 63 $FILE
 sleep $SLEEP_TIME && sync
-(echo n; echo p; echo 2; echo ; echo $SWAP_SIZE; echo t; echo 2; echo 82; echo w) | fdisk -H 255 -S 63 $FILE
+(echo n; echo p; echo 2; echo ; echo $SWAP_SIZE_F; echo t; echo 2; echo 82; echo w) | fdisk -H 255 -S 63 $FILE
 sleep $SLEEP_TIME && sync
 (echo n; echo p; echo 3; echo ; echo ; echo w) | fdisk -H 255 -S 63 $FILE
 sleep $SLEEP_TIME && sync
 
 DEV_NAME=$(losetup -f --show $FILE)
 DEV_INDEX=${DEV_NAME:${#DEV_NAME} - 1}
-DEV_BOOT="/dev/mapper/loop"$DEV_INDEX"p1"
-DEV_SWAP="/dev/mapper/loop"$DEV_INDEX"p2"
-DEV_ROOT="/dev/mapper/loop"$DEV_INDEX"p3"
+DEV_BOOT=/dev/loop$(expr $DEV_INDEX + 1)
+DEV_SWAP=/dev/loop$(expr $DEV_INDEX + 2)
+DEV_ROOT=/dev/loop$(expr $DEV_INDEX + 3)
+
+BOOT_OFFSET=$(expr $OFFSET)
+SWAP_OFFSET=$(expr $BOOT_OFFSET + $BOOT_SIZE)
+ROOT_OFFSET=$(expr $SWAP_OFFSET + $SWAP_SIZE)
 
 kpartx -a $DEV_NAME
+losetup --offset $BOOT_OFFSET $DEV_BOOT $DEV_NAME
+losetup --offset $SWAP_OFFSET $DEV_SWAP $DEV_NAME
+losetup --offset $ROOT_OFFSET $DEV_ROOT $DEV_NAME
 
 DEV_NAME=$DEV_NAME DEV_BOOT=$DEV_BOOT DEV_SWAP=$DEV_SWAP\
     DEV_ROOT=$DEV_ROOT SCHEMA=$SCHEMA LOADER=$LOADER $DIR/install.sh
+
+sync
+losetup -d $DEV_ROOT
+losetup -d $DEV_SWAP
+losetup -d $DEV_BOOT
 
 sync
 kpartx -d $DEV_NAME
