38a28361ae2d21329d2431debe6020fd36fdecf1
joamag <joamag@gmail.com>
2013-03-20 12:08:12 +0000

    new install structure

--- scripts/scudum/util/install.sh
+++ scripts/scudum/util/install.sh
@@ -54,22 +54,15 @@ case $LOADER in
         cat $SCUDUM/boot/grub/grub.cfg.tpl | sed -e "s/\${BOOT_FS}/$BOOT_FS/"\
             -e "s/\${ROOT_UUID}/$ROOT_UUID/" > $SCUDUM/boot/grub/grub.cfg
 
-        chroot $SCUDUM /usr/bin/env -i\
-            HOME=/root PATH=/bin:/usr/bin:/sbin:/usr/sbin\
-            DEV_NAME=$DEV_NAME grub-install $DEV_NAME
+        grub-install --root-directory=$SCUDUM/boot/grub $DEV_NAME
         ;;
 
     extlinux|isolinux)
         echo "tmpfs / tmpfs defaults 0 0" >> $SCUDUM/etc/fstab
 
-        chroot $SCUDUM /usr/bin/env -i\
-            HOME=/root PATH=/bin:/usr/bin:/sbin:/usr/sbin\
-            DEV_NAME=$DEV_NAME dd if=/usr/lib/syslinux/mbr.bin\
-            conv=notrunc bs=440 count=1 of=$DEV_NAME
-
-        chroot $SCUDUM /usr/bin/env -i\
-            HOME=/root PATH=/bin:/usr/bin:/sbin:/usr/sbin\
-            DEV_NAME=$DEV_NAME extlinux --install /boot
+        dd if=/usr/lib/syslinux/mbr.bin conv=notrunc\
+            bs=440 count=1 of=$PEN_NAME
+        extlinux --install $SCUDUM/boot
         ;;
 esac
 
