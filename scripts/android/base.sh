2fc05040ed0ac303a47c0bfcb975397cf88509f0
joamag <joamag@gmail.com>
2013-04-30 12:03:51 +0100

    small code change

--- scripts/android/base.sh
+++ scripts/android/base.sh
@@ -1,6 +1,6 @@
-export CFLAGS="-O3"
 export TOOLCHAIN="android-toolchain"
 export HOST="arm-linux-androideabi"
 export CROSS="arm-linux-"
 export BUILD="arm"
 export PREFIX=/opt/$TOOLCHAIN
+export CFLAGS="-O3 -L$PREFIX/lib -I$PREFIX/include"
