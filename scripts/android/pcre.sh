1a44571ccd768e35eba9faf0c989e0ee256a0380
joamag <joamag@gmail.com>
2013-04-30 10:18:07 +0100

    new build scripts for android

---
+++ scripts/android/pcre.sh
@@ -0,0 +1,11 @@
+VERSION="8.32"
+
+wget -q "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-$VERSION.tar.gz"
+tar -zxf "pcre-$VERSION.tar.gz"
+rm -f "pcre-$VERSION.tar.gz"
+cd pcre-$VERSION
+
+export PATH=$PREFIX/bin:$PATH
+./configure --host=$HOST --build=$BUILD --prefix=$PREFIX\
+    --disable-shared --enable-static --disable-cpp
+make && make install
