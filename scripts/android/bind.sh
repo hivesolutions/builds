bb4510acbb1420c651aa27707be7ab4f1b72ed2d
joamag <joamag@gmail.com>
2014-08-21 12:43:44 +0100

    new bind

--- scripts/android/bind.sh
+++ scripts/android/bind.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-9.4.1}
+VERSION=${VERSION-9.6.3}
 
 wget -q "http://ftp.isc.org/isc/bind9/$VERSION/bind-$VERSION.tar.gz"
 tar -zxf "bind-$VERSION.tar.gz"
@@ -8,6 +8,6 @@ cd bind-$VERSION
 wget -q "https://raw.github.com/hivesolutions/patches/master/config/config.sub" "--output-document=config.sub"
 export PATH=$PREFIX/bin:$PATH
 ./configure --host=$HOST --build=$BUILD --prefix=$PREFIX\
-    --enable-static --disable-shared --enable-libbind\
-    --disable-atomic --disable-linux-caps --enable-threads
+    --enable-static --enable-libbind --disable-atomic\
+    --disable-linux-caps --enable-threads
 make && make install
