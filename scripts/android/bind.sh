106f0c13ad4d1ac8833f0c3d68bea8853265f90a
joamag <joamag@gmail.com>
2014-08-21 12:38:57 +0100

    new code support

--- scripts/android/bind.sh
+++ scripts/android/bind.sh
@@ -5,6 +5,7 @@ tar -zxf "bind-$VERSION.tar.gz"
 rm -f "bind-$VERSION.tar.gz"
 cd bind-$VERSION
 
+wget -q "https://raw.github.com/hivesolutions/patches/master/config/config.sub" "--output-document=config.sub"
 export PATH=$PREFIX/bin:$PATH
 ./configure --host=$HOST --build=$BUILD --prefix=$PREFIX\
     --enable-static --disable-shared --enable-libbind\
