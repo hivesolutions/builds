1b6ee4a4848728fe43cf237ecdb8aec33f3a41b6
joamag <joamag@gmail.com>
2014-08-21 18:09:03 +0100

    new options

---
+++ scripts/android-ng/all.sh
@@ -0,0 +1,28 @@
+set +e
+
+rm -rf build
+mkdir build
+cd build
+
+../../rasp/deps.sh
+../../rasp/cross.sh
+
+source ../base.sh
+
+../../rasp/bin.sh
+../../rasp/openssl.sh
+../../rasp/pcre.sh
+../../rasp/zlib.sh
+../../rasp/zip.sh
+../../rasp/bz2.sh
+../../rasp/libpng.sh
+../../rasp/libjpeg.sh
+../../rasp/freetype.sh
+../../rasp/ncurses.sh
+../../rasp/readline.sh
+../../rasp/gmp.sh
+../../rasp/curl.sh
+../../rasp/python.sh
+../../rasp/lua.sh
+../../rasp/libxml2.sh
+../../rasp/php.sh
---
+++ scripts/android-ng/base.sh
@@ -0,0 +1,5 @@
+export CFLAGS="-O3 -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s"
+export HOST="arm-android-linux-gnueabi"
+export CROSS="arm-android-linux-"
+export BUILD="arm"
+export PREFIX=/opt/$HOST
