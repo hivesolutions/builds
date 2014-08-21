76adeab66f9d2e646e2f278a1d48496932f6eb05
joamag <joamag@gmail.com>
2014-08-21 10:48:52 +0100

    new comments

--- scripts/scudum/base.sh
+++ scripts/scudum/base.sh
@@ -1,8 +1,11 @@
 # exports a series of environment variables that
 # are going to be used through the build process
 export SCUDUM=${SCUDUM-/mnt/scudum}
-export LC_ALL=POSIX
 export SCUDUM_TARGET=$(uname -m)-scudum-linux-gnu
+
+# exposes the various build specific values so that
+# the initial value for the building is "tools based"
+export LC_ALL=POSIX
 export PATH=/tools/bin:/bin:/usr/bin
 export PREFIX=/tools
 
