bb13108cc9acadbe8c1b2085af783cb14487a12c
joamag <joamag@gmail.com>
2014-08-25 14:21:04 +0100

    new code support

--- scripts/scudum/system/tree.sh
+++ scripts/scudum/system/tree.sh
@@ -6,7 +6,7 @@ install -dv -m 1777 /tmp /var/tmp
 
 mkdir -pv /usr/{,local/}{bin,include,lib,sbin,src}
 mkdir -pv /usr/{,local/}share/{doc,info,locale,man}
-mkdir -v  /usr/{,local/}share/{misc,terminfo,zoneinfo}
+mkdir -pv /usr/{,local/}share/{misc,terminfo,zoneinfo}
 mkdir -pv /usr/{,local/}share/man/man{1..8}
 
 for dir in /usr /usr/local; do
@@ -17,7 +17,7 @@ case $(uname -m) in
     x86_64) ln -sv lib /lib64 && ln -sv lib /usr/lib64 ;;
 esac
 
-mkdir -v /var/{log,mail,spool}
+mkdir -pv /var/{log,mail,spool}
 ln -sv /run /var/run
 ln -sv /run/lock /var/lock
 mkdir -pv /var/{opt,cache,lib/{misc,locate},local}
