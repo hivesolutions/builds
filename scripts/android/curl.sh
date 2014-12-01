402c7b64a507c6e282ccb29a1aaeac34f1728782
joamag <joamag@gmail.com>
2014-12-01 20:24:53 +0000

    new code

--- scripts/android/curl.sh
+++ scripts/android/curl.sh
@@ -7,12 +7,17 @@ tar -zxf "curl-$VERSION.tar.gz"
 rm -f "curl-$VERSION.tar.gz"
 cd curl-$VERSION
 
+unset CFLAGS
+
 wget "https://raw.github.com/hivesolutions/patches/master/config/config.sub" "--output-document=config.sub"
 export PATH=$PATH:$PREFIX/bin
 export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib\
+    -R$PREFIX/lib\
     -L$PREFIX/usr/lib\
-    -L$PREFIX/$HOST/sysroot/usr/lib"
+    -R$PREFIX/usr/lib\
+    -L$PREFIX/$HOST/sysroot/usr/lib\
+    -R$PREFIX/$HOST/sysroot/usr/lib"
 ./configure --host=$HOST --build=$BUILD --prefix=$PREFIX\
     --disable-shared --enable-ipv6 --with-ssl --with-zlib
 make && make install
