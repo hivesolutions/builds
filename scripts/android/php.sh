c19f709ce4ed02be6fbe4c377b791c94c7a519ce
joamag <joamag@gmail.com>
2014-12-01 19:33:44 +0000

    new code

--- scripts/android/php.sh
+++ scripts/android/php.sh
@@ -16,6 +16,6 @@ export CFLAGS="$CFLAGS -I$PREFIX/include\
 export EXTRA_CFLAGS="-lresolv"
 ./configure --host=$HOST --build=$BUILD --prefix=$PREFIX\
     --enable-embed=static --enable-bcmath --disable-sockets --disable-phar\
-    --without-pear --without-iconv --with-libxml-dir=$PREFIX\
+    --disable-posix --without-pear --without-iconv --with-libxml-dir=$PREFIX\
     --with-config-file-path=/usr/lib
 make && make install
