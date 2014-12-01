aca80d724deec73f974b1503d82b91ea361a045f
joamag <joamag@gmail.com>
2014-12-01 20:20:24 +0000

    new curl lib

---
+++ scripts/android/curl.sh
@@ -0,0 +1,21 @@
+VERSION=${VERSION-7.29.0}
+
+set -e +h
+
+wget "http://curl.haxx.se/download/curl-$VERSION.tar.gz"
+tar -zxf "curl-$VERSION.tar.gz"
+rm -f "curl-$VERSION.tar.gz"
+cd curl-$VERSION
+
+wget "https://raw.github.com/hivesolutions/patches/master/config/config.sub" "--output-document=config.sub"
+export PATH=$PATH:$PREFIX/bin
+export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib\
+    -R$PREFIX/lib\
+    -L$PREFIX/usr/lib\
+    -R$PREFIX/usr/lib\
+    -L$PREFIX/$HOST/sysroot/usr/lib\
+    -R$PREFIX/$HOST/sysroot/usr/lib"
+./configure --host=$HOST --build=$BUILD --prefix=$PREFIX\
+    --disable-shared --enable-ipv6 --with-ssl --with-zlib
+make && make install
